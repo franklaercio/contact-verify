1ac61b84ad6c2e8c838890491f5099b98dda69f6
Frank <franklaercio@gmail.com>
2024-12-06 00:50:32 -0300

    fix: adjusted dependencies of postgresql

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.r2dbc:r2dbc-postgresql")
+	implementation("io.r2dbc:r2dbc-postgresql:0.8.13.RELEASE")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
